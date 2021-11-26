cff69f9b6ba095d6980a2deb7f9f74bb13e737bf
Aniket-26J <aniketjagtap62@gmail.com>
2021-11-26 12:10:20 +0530

    first commit

---
+++ src/test/java/myrunner/TestRunner.java
@@ -0,0 +1,30 @@
+package myrunner;
+
+
+
+import org.junit.runner.RunWith;
+
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "./src\\test\\java\\features",
+		
+		
+		
+		glue = "stepdefination"
+
+)
+
+
+
+public class TestRunner {
+
+
+
+}
